3b028a7ce99c8e83953eff41f42126125204a508
Guilherme Staffen <53901092+GuilhermeStaffen@users.noreply.github.com>
2024-11-06 20:16:33 -0300

    Update TesteBiblioteca.java

--- criador/src/TesteBiblioteca.java
+++ criador/src/TesteBiblioteca.java
@@ -9,6 +9,8 @@ public class TesteBiblioteca {
         Livro livro1 = new Livro("O Senhor dos Anéis");
         Livro livro2 = new Livro("1984");
 
+        //Entendemos que o objeto EMPRESTIMO deveria ser criado nesta classe. Criamos um objeto para cada livro emprestado
+        // e comunicamos a classe BIBLIOTECA que este emprestimo ocorreu.
         Emprestimo emprestimo1 = new Emprestimo(livro1, "João");
         Emprestimo emprestimo2 = new Emprestimo(livro2, "Maria");
         biblioteca.registrarEmprestimo(emprestimo1);
