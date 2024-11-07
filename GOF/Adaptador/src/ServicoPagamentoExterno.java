73a96eaf9f07f5c977bb9b1ed341665331af3a8d
Guilherme Staffen <guilhermestaffen@gmail.com>
2024-11-06 21:32:07 -0300

    Ajustando organização das soluções

---
+++ GOF/Adaptador/src/ServicoPagamentoExterno.java
@@ -0,0 +1,9 @@
+// Interface externa de serviço de pagamento
+
+class ServicoPagamentoExterno {
+
+    public void pagarFatura(String identificador, double valor) {
+        System.out.println("Pagamento de R$ " + valor + " processado para o identificador: " + identificador);
+    }
+    
+}
